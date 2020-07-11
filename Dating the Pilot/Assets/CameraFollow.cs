156c868152ce1b7bf841ee8f1eba8ae4fd7452bb
Esosek <esonicitel@gmail.com>
2020-07-11 12:52:58 +0200

    init

---
+++ Dating the Pilot/Assets/CameraFollow.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform objectToFollow;
+    public Vector3 offset;
+
+    void Update()
+    {
+        transform.position = objectToFollow.position + offset;
+    }
+}
---
+++ Dating the Pilot/Assets/PlaneMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneMovement : MonoBehaviour
+{
+    public float speed = 1000f;
+    public float slowingSpeed = 1f;
+    public float gravityModifier = 0.5f;
+    float mouseXInput;
+    float mouseYInput;
+
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        speed -= Time.deltaTime * slowingSpeed * 100f;
+        //Debug.Log("Plane: Current speed is " + currentSpeed.ToString("0"));
+    }
+
+    private void Start()
+    {
+        //forward force
+        rb.AddForce(transform.forward * speed);
+    }
+
+    private void FixedUpdate()
+    {
+        
+
+        //static up force
+        rb.AddForce(Vector3.up * Time.fixedDeltaTime * 10000f / gravityModifier);
+    }
+}

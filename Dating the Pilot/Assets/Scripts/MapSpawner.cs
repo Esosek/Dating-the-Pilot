4f14ed4e9a3703c042cfdcc80373c716248108d6
Esosek <67476790+Esosek@users.noreply.github.com>
2020-07-11 19:45:30 +0200

    RB plane, level generation

---
+++ Dating the Pilot/Assets/Scripts/MapSpawner.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapSpawner : MonoBehaviour
+{
+    public GameObject[] tiles;
+    public GameObject landingTile;
+    public Transform firstTilePos;
+
+    public int maxCol;
+    public int maxRow;
+
+    bool landingSpawned;
+    int[] rotations = { 0, 90, 180, 270 };
+
+    void Start()
+    {
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        float offset = 400f;
+        Vector3 currentSpawnPoint = firstTilePos.position;
+
+        for (int i = 0; i < maxRow; i++)
+        {
+            if(!landingSpawned && i == 7)
+            {
+                int landingIndex = Random.Range(0, maxCol);
+                // landing possible points
+                for (int o = 0; o < maxCol; o++)
+                {
+                    if(o == landingIndex)
+                    {
+                        Instantiate(landingTile, new Vector3(currentSpawnPoint.x + offset * o, currentSpawnPoint.y, currentSpawnPoint.z), Quaternion.identity);
+                        landingSpawned = true;
+                    }
+                    else
+                    {
+                        int _randomTileIndex = Random.Range(0, tiles.Length);
+                        Instantiate(tiles[_randomTileIndex], new Vector3(currentSpawnPoint.x + offset * o, currentSpawnPoint.y, currentSpawnPoint.z), Quaternion.AngleAxis(rotations[Random.Range(0, rotations.Length)], Vector3.up));
+                    } 
+                }
+            }
+
+            else
+            {
+                for (int o = 0; o < maxCol; o++)
+                {
+                    int _randomTileIndex = Random.Range(0, tiles.Length);
+                    Instantiate(tiles[_randomTileIndex], new Vector3(currentSpawnPoint.x + offset * o, currentSpawnPoint.y, currentSpawnPoint.z), Quaternion.AngleAxis(rotations[Random.Range(0, rotations.Length)], Vector3.up));
+                }
+            }
+
+            currentSpawnPoint = new Vector3(currentSpawnPoint.x, currentSpawnPoint.y, currentSpawnPoint.z + offset);
+        }
+
+        Debug.Log("Map spawned");
+    }
+}
---
+++ Dating the Pilot/Assets/Scripts/PlaneMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaneMovement : MonoBehaviour
+{
+    public float speed = 1000f;
+    public float slowingSpeed = 1f;
+    public float rotationSpeed = 5f;
+    public float maxModelRotation = 2f;
+    public float modelRotationSpeed = 1f;
+    public float gravityModifier = 0.5f;
+    public Transform planeModel;
+    public Transform crosshair;
+
+    Vector2 screenCenter;
+    Vector2 mouseDistance;
+    Vector2 lookInput;
+
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Start()
+    {
+        screenCenter.x = Screen.width / 2;
+        screenCenter.y = Screen.height / 2;
+
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        speed -= Time.deltaTime * slowingSpeed * 100f;
+        //Debug.Log("Plane: Current speed is " + currentSpeed.ToString("0"));
+
+        Tilting();
+    }
+
+    private void FixedUpdate()
+    {
+        //forward force
+        rb.AddForce(transform.forward * speed * Time.fixedDeltaTime);
+
+        //static up force
+        rb.AddForce(Vector3.up * Time.fixedDeltaTime * 10000f / gravityModifier);
+    }
+
+    void Tilting()
+    {
+        lookInput = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+        mouseDistance = new Vector2((lookInput.x - screenCenter.x) / screenCenter.y, (lookInput.y - screenCenter.y) / screenCenter.y);
+
+        mouseDistance = Vector2.ClampMagnitude(mouseDistance, 1f);
+
+        // crosshair move
+        crosshair.localPosition = new Vector3 (mouseDistance.x * 100f, mouseDistance.y * 100f, 0f);
+
+        // model rotation
+        Quaternion targetRotation = Quaternion.Euler (-mouseDistance.y * maxModelRotation, mouseDistance.x * maxModelRotation, -mouseDistance.x * maxModelRotation);
+        planeModel.localRotation = Quaternion.Lerp(planeModel.localRotation, targetRotation, Time.deltaTime * modelRotationSpeed);
+
+        transform.Rotate(-mouseDistance.y * rotationSpeed * Time.deltaTime, mouseDistance.x * rotationSpeed * Time.deltaTime, 0f, Space.Self);
+    }
+}
